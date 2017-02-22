6710db57ee06dea7bf2cbf6aed7f5f6cf2b00d6e
Adam Saxén <adam.saxen@gmail.com>
2017-02-22 09:50:25 +0100

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # npm-package-logger
+The logger package is used for configuring the winston logging module.
+
+## How to use
+
+Load the module and set the logging configuration arguments.
+```js
+var Logger = require('ioant-logger');
+// setup(path_to_log_folder, log_level_test, log_level_production, log_level_console)
+Logger.setup('logs/', 'debug', 'debug', 'debug');
+```
+Available log levels are:
+- error
+- warn
+- info
+- verbose
+- debug
+- silly
+
+Example of an application logging all logs of **warn** and above to file in production. When testing we wan't to store everything to file but only errors to console
+```js
+var Logger = require('ioant-logger');
+// setup(path_to_log_folder, log_level_test, log_level_production, log_level_console)
+Logger.setup('logs/', 'silly', 'warn', 'error');
+```
---
+++ logger.js
@@ -0,0 +1,32 @@
+/**
+ * @file logger.js
+ * @author Adam Saxén
+ *
+ *  Module for logging 
+ */
+
+var winston = require('winston');
+var path = require('path');
+
+module.exports = winston;
+
+module.exports.setup = function(log_path, log_level_test, log_level_production, log_level_console) {
+    log_path_ = path.join(process.cwd(), log_path);
+    if (process.env.NODE_ENV !== 'test') {
+        winston = new (winston.Logger)({
+            transports: [
+                new (winston.transports.Console)({ level: log_level_console }),
+                new (winston.transports.File)({ filename: path.join(log_path_, 'output.log'), level: log_level_production })
+            ]
+        });
+    } else {
+        // while testing, log only to file, leaving stdout free for unit test status messages
+        winston = new (winston.Logger)({
+            transports: [
+                new (winston.transports.Console)({ level: log_level_console}),
+                new (winston.transports.File)({ filename:path.join(log_path_, 'output_tests.log') , level: log_level_test })
+            ]
+        });
+    }
+    module.exports = winston;
+};
